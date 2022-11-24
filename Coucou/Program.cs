82ce875f68fed2302a37193af5e8dd88ec8d12a3
Le coq <Amos.LE-COQ@cpnv.ch>
2022-11-24 09:06:00 +0100

    Add project files.

---
+++ Coucou/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Coucou;
+
+Console.WriteLine("lettre:");
+
+rep r = new rep("Coucou je suis A", "Coucou je suis A et B", "Je suis malade avec certificat");
+rep salut = new List<rep>();
+
+string lettre = Console.ReadLine();
+
+switch (lettre)
+{
+	case "A":
+	Console.WriteLine(r.A);
+	break;
+        
+	case "B":
+	Console.WriteLine(r.B);
+	break;
+        
+	case "C":
+	Console.WriteLine(r.C);
+	break;
+        
+	default:
+	break;
+}
---
+++ Coucou/rep.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coucou
+{
+    public class rep
+    {
+        #region private
+        private string _a;
+        #endregion private
+
+        #region public
+        public rep(string a)
+        {
+            _a = a;
+        }
+
+        public string A
+        {
+            get
+            {
+                return _a;
+            }
+        }
+        #endregion public
+    }
+}
